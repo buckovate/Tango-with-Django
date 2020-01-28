7b3f73827b32e4015271a16112cc200b15133acc
Tereza Buckova (student) <2200528B@student.gla.ac.uk>
2020-01-28 15:04:06 +0000

    all of this

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-def index (request):
-    return HttpResponse("Rango says hi")
 
+def about(request):
+	return HttpResponse("hello")
+
+def index(request):
+	return HttpResponse("hello")
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context=context_dict)
