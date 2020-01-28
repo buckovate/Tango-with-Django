bc0bd1158eb4de0eb5ec1b12536fc59602a1fb60
Tereza Buckova (student) <2200528B@student.gla.ac.uk>
2020-01-28 16:31:48 +0000

    3rd chapter

--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about(request):
-	return HttpResponse("Rango says here is the about page. \
-        <br/> <a href='/rango/'>index</a>")
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render (request, 'rango/about.html', context=context_dict)
 	
 	
-	
-
 def index(request):
 # Construct a dictionary to pass to the template engine as its context.
 # Note the key boldmessage matches to {{ boldmessage }} in the template!
