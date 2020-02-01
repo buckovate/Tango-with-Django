d401937773817a8efb7f279e0c85c21c84e37810
buckovate <buckovate@gmail.com>
2020-02-01 11:15:05 +0000

    Update models.py

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -9,11 +12,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING,)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
     
-    def __str__(self): # For Python 2, use __unicode__ too
+    def __str__(self):  # For Python 2, use __unicode__ too
         return self.title
