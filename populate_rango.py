5593aa5916b4c1e117eedd085e28ea71318a7367
buckovate <buckovate@gmail.com>
2020-02-02 19:13:13 +0000

    chap 6

--- populate_rango.py
+++ populate_rango.py
@@ -14,29 +14,24 @@ def populate():
 # through each data structure, and add the data to our models.
 
     python_pages = [
-        {"title": "Official Python Tutorial",
-        "url":"http://docs.python.org/2/tutorial/"},
-        {"title":"How to Think like a Computer Scientist",
-        "url":"http://www.greenteapress.com/thinkpython/"},
-        {"title":"Learn Python in 10 Minutes",
-        "url":"http://www.korokithakis.net/tutorials/python/"} ]
-
+		{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/", "views": 32},
+		{"title":"How to Think like a Computer Scientist", "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+	    {"title":"Learn Python in 10 Minutes", "url":"http://www.korokithakis.net/tutorials/python/", "views": 8},]
+        
     django_pages = [
-        {"title":"Official Django Tutorial",
-        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
-        {"title":"Django Rocks",
-        "url":"http://www.djangorocks.com/"},
-        {"title":"How to Tango with Django",
-        "url":"http://www.tangowithdjango.com/"} ]
-
-    other_pages = [
-        {"title":"Bottle",
-        "url":"http://bottlepy.org/docs/dev/"},
-        {"title":"Flask",
-        "url":"http://flask.pocoo.org"} ]
+		{"title":"Official Django Tutorial",
+	        "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
+		{"title":"Django Rocks",
+	        "url":"http://www.djangorocks.com/" , "views": 16},
+		{ "title":"How to Tango with Django",
+	        "url":"http://www.tangowithdjango.com/", "views": 8}  ]
+        
+    other_pages =  [
+		{ "title":"Bottle", "url":"http://bottlepy.org/docs/dev/", "views": 32},
+		{ "title":"Flask", "url":"http://flask.pocoo.org", "views": 16}  ]
 
     cats = {
-        "Python": {"pages": python_pages, "views":128, "likes":64},
+        "Python": {"pages": python_pages, "views":138, "likes":64},
         "Django": {"pages": django_pages, "views":64, "likes":32},
         "Other Frameworks": {"pages": other_pages, "views":32,"likes":16},
  }
--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rango import views
 app_name = 'rango'
+from django.conf.urls import url
 
-urlpatterns = [ path('', views.index, name='index'),
-				path ('about/', views.about, name='about'),	 
-            ]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+ ]
